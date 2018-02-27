d8611156a3eeb8af994163a750f6cdd063787448
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-02-27 09:40:56 +0700

    Added basic modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+// Main app load
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './components/App';
 import registerServiceWorker from './config/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React redux related imports
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// React router related imports
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render((
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+	),
+	document.getElementById('root')
+);
+
 registerServiceWorker();
