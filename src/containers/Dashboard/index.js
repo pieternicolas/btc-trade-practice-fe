5f06a8cebad0f290ad7234aeb7ae978b756c1bba
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-03-22 14:14:02 +0700

    Fixed squished main view on sidebar mobile

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from './../../components/Header';
@@ -6,20 +7,34 @@ import Sidebar from './../../components/Sidebar';
 
 import Entry from './../Entry';
 
-const index = (props) => {
-  return (
-    <div className="h-100 w-100">
-    	<Header></Header>
+import './styles/index.css';
 
-    	<Sidebar></Sidebar>
 
-			<div className="container-fluid pt-3">
-				<Switch>
-					<Route path="/dashboard" exact component={Entry}></Route>
-				</Switch>
+function mapStateToProps(state) {
+	return {
+		isActive: state.styling.sidebarActive
+	};
+}
+
+export class Dashboard extends React.Component {
+	render() {
+		return (
+			<div className="h-100 w-100">
+				<Header></Header>
+
+				<Sidebar></Sidebar>
+
+				<div className={`container-fluid pt-3 ${this.props.isActive ? 'sidebar-active' : ''}`}>
+					<Switch>
+						<Route path="/dashboard" exact component={Entry}></Route>
+					</Switch>
+				</div>
 			</div>
-    </div>
-  );
-};
+		);
+	}
+}
 
-export default index;
+export default connect(
+	mapStateToProps,
+// Implement map dispatch to props
+)(Dashboard)
